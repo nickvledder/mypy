9ec82c141bd44b9d97af563e1ae56e2b36338385
Nick Vledder <nickvledder@gmail.com>
2019-07-08 11:03:29 +0200

    Update my.py

--- my.py
+++ my.py
@@ -1,3 +1,6 @@
-print "Content-Type: text/plain"
-print ""
-print "Congratulations, it's a web app!"
+from mod_python import apache
+
+def handler(req):
+    req.content_type = 'text/plain'
+    req.write("Hello, World!")
+    return apache.OK
